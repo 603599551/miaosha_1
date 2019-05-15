88e4b23c73f8d13c7433142f35efe8812a3c8bc8
CaryZ <603599551@qq.com>
2019-05-15 23:23:26 +0800

    对象类：将商品表和秒杀商品表的数据合到一起

---
+++ src/main/java/com/example/demo/vo/GoodsVo.java
@@ -0,0 +1,50 @@
+package com.example.demo.vo;
+
+import com.example.demo.domain.Goods;
+
+import java.util.Date;
+
+/**
+ * @date 2019-05-13
+ * 将商品表和秒杀商品表的数据合到一起
+ */
+public class GoodsVo extends Goods {
+
+    private Double miaoshaPrice;
+    private Integer stockCount;
+    private Date startDate;
+    private Date endDate;
+
+    public Integer getStockCount() {
+        return stockCount;
+    }
+
+    public void setStockCount(Integer stockCount) {
+        this.stockCount = stockCount;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Double getMiaoshaPrice() {
+        return miaoshaPrice;
+    }
+
+    public void setMiaoshaPrice(Double miaoshaPrice) {
+        this.miaoshaPrice = miaoshaPrice;
+    }
+
+}
