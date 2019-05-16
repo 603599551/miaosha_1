f6cd9c05bfa0d335a5fe1a042a2e8cad6e42cd62
CaryZ <603599551@qq.com>
2019-05-16 15:37:11 +0800

    添加订单模块的异常信息种类

--- src/main/java/com/example/demo/result/CodeMsg.java
+++ src/main/java/com/example/demo/result/CodeMsg.java
@@ -22,7 +22,8 @@ public class CodeMsg {
     //订单模块 5004XX
 
     //秒杀模块 5005XX
-
+    public static CodeMsg MIAOSHA_OVER=new CodeMsg(500500,"商品已经秒杀完毕");
+    public static CodeMsg REPEAT_MIAOSHA_ERROR=new CodeMsg(500501,"商品不能重复秒杀");
 
     private CodeMsg(int code,String msg){
         this.code=code;
