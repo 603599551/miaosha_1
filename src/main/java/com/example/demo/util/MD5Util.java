0fd64d7ebc6cc899f106b20b51b3d04379173dcc
CaryZ <603599551@qq.com>
2019-05-18 11:42:04 +0800

    解决bug ： md2 -> md5

--- src/main/java/com/example/demo/util/MD5Util.java
+++ src/main/java/com/example/demo/util/MD5Util.java
@@ -6,19 +6,18 @@ import org.apache.commons.codec.digest.DigestUtils;
 public class MD5Util {
 
     public static String md5(String str){
-        return DigestUtils.md2Hex(str);
+        return DigestUtils.md5Hex(str);
     }
 
     //固定salt，“拼装”用户端输入的明文密码，整体再做md5
     private static final String salt="1a2b3c4d";
 
     /**
-     * @deprecated 用户端 采用js提供的MD5算法，不用本方法
      * 第一次MD5 用户端：PASSWORD1=MD5(密码明文+固定salt)
      * @param inputPwd
      * @return
      */
-    public static String inputPwdToFormPwd(String inputPwd){
+    public static String inputPassToFormPass(String inputPwd){
         String str= ""+salt.charAt(0)+salt.charAt(2) + inputPwd +salt.charAt(5) + salt.charAt(4);
         return md5(str);
     }
@@ -30,26 +29,26 @@ public class MD5Util {
      * @param salt
      * @return
      */
-    public static String formPwdToDBPwd(String formPwd,String salt){
+    public static String formPassToDbPass(String formPwd, String salt){
         String str= ""+salt.charAt(0)+salt.charAt(2) + formPwd +salt.charAt(5) + salt.charAt(4);
         return md5(str);
     }
 
-//    /**
-//     * 两次MD5
-//     * @param inputPwd
-//     * @param saltDB
-//     * @return
-//     */
-//    public static String inputPwdToDBPwd(String inputPwd,String saltDB){
-//        String formPwd=inputPwdToFormPwd(inputPwd);
-//        String dbPwd=formPwdToDBPwd(formPwd,saltDB);
-//        return dbPwd;
-//    }
+    /**
+     * 两次MD5
+     * @param inputPwd
+     * @param saltDB
+     * @return
+     */
+    public static String inputPassToDbPass(String inputPwd, String saltDB){
+        String formPwd= inputPassToFormPass(inputPwd);
+        String dbPwd= formPassToDbPass(formPwd,saltDB);
+        return dbPwd;
+    }
 
     public static void main(String []args){
-//        System.out.println(inputPwdToFormPwd("123456"));
-//        System.out.println(formPwdToDBPwd("d3b1294a61a07da9b49b6e22b2cbd7f9","1a2b3c4d"));
-//        System.out.println(inputPwdToDBPwd("123456","1a2b3c4d"));
+//        System.out.println(inputPassToFormPass("123456"));
+//        System.out.println(formPassToDbPass("d3b1294a61a07da9b49b6e22b2cbd7f9","1a2b3c4d"));
+//        System.out.println(inputPassToDbPass("123456","1a2b3c4d"));
     }
 }
